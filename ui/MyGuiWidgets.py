21ec0b43180069c96e53a662a7dd6bc149e0c790
StanislavAntunovich <stanislav.antunovich@yandex.ru>
2018-09-01 17:24:56 +0300

    refactoring

---
+++ ui/MyGuiWidgets.py
@@ -0,0 +1,125 @@
+from PyQt5 import QtGui, QtCore, QtWidgets
+from os.path import abspath, join
+
+STRING_HIGH = 24
+CONTACT_BACKGROUND_COLOR = 'light green'
+NON_CONTACT_BACKGROUND_COLOR = 'yellow'
+NO_COLOR = 'white'
+
+
+class MyListWidget(QtWidgets.QListWidget):
+    del_contact_signal = QtCore.pyqtSignal(str)
+    add_contact_signal = QtCore.pyqtSignal(str)
+    remove_and_ignore_signal = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.RightButton:
+            if event.y() < self.count() * STRING_HIGH:
+                self.setCurrentRow(event.y() // STRING_HIGH)
+                if self.currentItem().in_contacts:
+                    self.context_menu_for_contacts(event)
+                else:
+                    self.context_menu_for_non_contacts(event)
+        super().mousePressEvent(event)
+
+    def context_menu_for_contacts(self, event):
+        menu = QtWidgets.QMenu(self)
+        menu.addAction('delete', self.context_del_contact)
+        menu.exec(event.globalPos())
+
+    def context_menu_for_non_contacts(self, event):
+        menu = QtWidgets.QMenu(self)
+        menu.addAction('add contact', self.context_add_contact)
+        menu.addSeparator()
+        menu.addAction('remove and ignore', self.context_remove_and_ignore)
+        menu.exec(event.globalPos())
+
+    def context_add_contact(self):
+        name = self.currentItem().text()
+        self.add_contact_signal.emit(name)
+
+    def contact_added(self, contact_item):
+        contact = self.currentItem() if not contact_item else contact_item
+        if contact.background() == QtGui.QColor(NON_CONTACT_BACKGROUND_COLOR):
+            contact.setBackground(QtGui.QColor(CONTACT_BACKGROUND_COLOR))
+
+    def context_del_contact(self):
+        name = self.currentItem().text()
+        self.del_contact_signal.emit(name)
+
+    def context_remove_and_ignore(self):
+        name = self.currentItem().text()
+        self.takeItem(self.currentRow())
+        self.remove_and_ignore_signal.emit(name)
+
+    def new_message_icon(self, item_):
+        item_.messages_count += 1
+        if item_.messages_count <= 10:
+            icon_file = '{}.png'.format(item_.messages_count)
+            icon = join(abspath('.'), 'ui', 'numbers_icons', icon_file)
+        else:
+            icon = join(abspath('.'), 'ui', 'numbers_icons', '11')
+        item_.setIcon(QtGui.QIcon(icon))
+
+    def new_message(self, name):
+        item = self.findItems(name, QtCore.Qt.MatchExactly)
+        if not item:
+            new_contact = self.addItem(name)
+            new_contact.setBackground(QtGui.QColor(NON_CONTACT_BACKGROUND_COLOR))
+            self.new_message_icon(new_contact)
+        else:
+            contact = item[0]
+            if contact.in_contacts:
+                contact.setBackground(QtGui.QColor(CONTACT_BACKGROUND_COLOR))
+            else:
+                contact.setBackground(QtGui.QColor(NON_CONTACT_BACKGROUND_COLOR))
+            self.new_message_icon(contact)
+
+    def item_double_clicked(self):
+        self.currentItem().messages_count = 0
+        self.currentItem().setBackground(QtGui.QColor(NO_COLOR))
+        self.currentItem().setIcon(QtGui.QIcon(''))
+
+    def add_contact(self, contact_name):
+        contact = self.findItems(contact_name, QtCore.Qt.MatchExactly)
+        if contact:
+            new_contact = contact[0]
+            self.contact_added(new_contact)
+        else:
+            new_contact = self.addItem(contact_name)
+        new_contact.in_contacts = True
+
+    def add_contacts(self, contacts_list):
+        for contact in contacts_list:
+            self.add_contact(contact)
+
+    def addItem(self, *__args):
+        item = MyListItem(*__args)
+        super().addItem(item)
+        print(item.sizeHint())
+        return item
+
+
+class MyListItem(QtWidgets.QListWidgetItem):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.messages_count = 0
+        self.in_contacts = False
+        # self.setSizeHint(QtCore.QSize(-1, STRING_HIGH))
+
+
+class MyTextEditWidget(QtWidgets.QTextEdit):
+    send_message_signal = QtCore.pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.send_message_shortcut_id = self.grabShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_Return))
+
+    def event(self, event_):
+        if event_.type() == QtCore.QEvent.Shortcut:
+            if event_.shortcutId() == self.send_message_shortcut_id:
+                self.send_message_signal.emit()
+        return super().event(event_)
---
+++ ui/main_window.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\mainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from ui.MyGuiWidgets import *
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(997, 647)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.ContactsCountLabel = QtWidgets.QLabel(self.centralwidget)
+        self.ContactsCountLabel.setGeometry(QtCore.QRect(700, 110, 81, 20))
+        self.ContactsCountLabel.setObjectName("ContactsCountLabel")
+        self.contactsListWidget = MyListWidget(self.centralwidget)
+        self.contactsListWidget.setGeometry(QtCore.QRect(700, 140, 261, 361))
+        self.contactsListWidget.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.contactsListWidget.setAcceptDrops(True)
+        self.contactsListWidget.setObjectName("contactsListWidget")
+
+        self.ChatBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.ChatBrowser.setGeometry(QtCore.QRect(30, 100, 641, 421))
+        self.ChatBrowser.setObjectName("ChatBrowser")
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.ChatBrowser.setFont(font)
+        self.addContactPushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.addContactPushButton.setGeometry(QtCore.QRect(890, 80, 61, 28))
+        self.addContactPushButton.setObjectName("addContactPushButton")
+        self.userNameLabel = QtWidgets.QLabel(self.centralwidget)
+        self.userNameLabel.setGeometry(QtCore.QRect(340, 10, 321, 20))
+        self.userNameLabel.setTextFormat(QtCore.Qt.AutoText)
+        self.userNameLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.userNameLabel.setObjectName("userNameLabel")
+        self.chatTextLabel = QtWidgets.QLabel(self.centralwidget)
+        self.chatTextLabel.setGeometry(QtCore.QRect(40, 70, 251, 20))
+        self.chatTextLabel.setObjectName("chatTextLabel")
+        self.addContactLineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.addContactLineEdit.setGeometry(QtCore.QRect(700, 80, 181, 27))
+        self.addContactLineEdit.setObjectName("addContactLineEdit")
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(30, 550, 541, 31))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.messageSendTextEdit = MyTextEditWidget(self.widget)
+        self.messageSendTextEdit.setObjectName("messageSendTextEdit")
+        self.horizontalLayout.addWidget(self.messageSendTextEdit)
+        self.sendMessagePushButton = QtWidgets.QPushButton(self.widget)
+        self.sendMessagePushButton.setObjectName("sendMessagePushButton")
+        self.horizontalLayout.addWidget(self.sendMessagePushButton)
+
+        self.nonContactsWidget = QtWidgets.QWidget(self.centralwidget)
+        self.nonContactsWidget.setGeometry(QtCore.QRect(710, 520, 241, 31))
+        self.nonContactsWidget.setObjectName("nonContactsWidget")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.nonContactsWidget)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.addNonContactPushButton = QtWidgets.QPushButton(self.nonContactsWidget)
+        self.addNonContactPushButton.setObjectName("addNonContactPushButton")
+        self.horizontalLayout_2.addWidget(self.addNonContactPushButton)
+        self.ignoreNonContactPushButton = QtWidgets.QPushButton(self.nonContactsWidget)
+        self.ignoreNonContactPushButton.setObjectName("ignoreNonContactPushButton")
+        self.horizontalLayout_2.addWidget(self.ignoreNonContactPushButton)
+
+        self.contactsWidget = QtWidgets.QWidget(self.centralwidget)
+        self.contactsWidget.setGeometry(QtCore.QRect(710, 520, 241, 31))
+        self.contactsWidget.setObjectName("contactsWidget")
+        self.horizontalLayout_33 = QtWidgets.QHBoxLayout(self.contactsWidget)
+        self.horizontalLayout_33.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_33.setObjectName("horizontalLayout_33")
+        self.deleteContactPushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.deleteContactPushButton.setGeometry(QtCore.QRect(710, 520, 241, 31))
+        self.deleteContactPushButton.setObjectName("deleteContactPushButton")
+        self.horizontalLayout_33.addWidget(self.deleteContactPushButton)
+
+        self.contactsActionsBox = QtWidgets.QStackedWidget(self.centralwidget)
+        self.contactsActionsBox.setGeometry(QtCore.QRect(710, 520, 241, 31))
+        self.contactsActionsBox.addWidget(self.contactsWidget)
+        self.contactsActionsBox.addWidget(self.nonContactsWidget)
+        self.contactsActionsBox.setCurrentWidget(self.contactsWidget)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 618, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Messenger"))
+        self.ContactsCountLabel.setText(_translate("MainWindow", "Total: "))
+        self.deleteContactPushButton.setText(_translate("MainWindow", "Delete"))
+        self.addContactPushButton.setText(_translate("MainWindow", "Add"))
+        self.userNameLabel.setText(_translate("MainWindow", "Self"))
+        self.chatTextLabel.setText(_translate("MainWindow", "Chat With:"))
+        self.addContactLineEdit.setPlaceholderText(_translate("MainWindow", "contact name to add"))
+        self.messageSendTextEdit.setPlaceholderText(_translate("MainWindow", "type message here "))
+        self.sendMessagePushButton.setText(_translate("MainWindow", "Send Message"))
+        self.addNonContactPushButton.setText(_translate("MainWindow", "Add"))
+        self.ignoreNonContactPushButton.setText(_translate("MainWindow", "Ignore"))
+
