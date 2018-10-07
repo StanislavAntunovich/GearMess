0a1ca36ec391cb22b1cd3a3a761f942e05c4f1a1
StanislavAntunovich <stanislav.antunovich@yandex.ru>
2018-10-07 18:26:08 +0300

    убрано отображение сообщений от себя при рассылке всем

--- ui/receiver.py
+++ ui/receiver.py
@@ -18,7 +18,8 @@ class Receiver(QtCore.QThread):
             message = self.parent().user.receive()
             if message:
                 if message.get(MESSAGE):
-                    self.new_message_signal.emit(message)
-                    self.parent().trayIcon.showMessage('GearMess \nNew message', f'From: {message[FROM]}')
+                    if message[FROM] != self.user.name:
+                        self.new_message_signal.emit(message)
+                        self.parent().trayIcon.showMessage('GearMess \nNew message', f'From: {message[FROM]}')
                 else:
                     self.service_queue.put(message)
