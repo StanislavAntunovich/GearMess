da178aa0783e5c002a9e4ba0dda2033cde0b6643
StanislavAntunovich <39097798+StanislavAntunovich@users.noreply.github.com>
2018-10-11 20:24:47 +0300

    добавлен игнор лист
    пока не используется

--- client_src/mongo_db/mongo_handlers.py
+++ client_src/mongo_db/mongo_handlers.py
@@ -1,4 +1,4 @@
-from client_src.mongo_db.mongo_model import messages_history_collection, contacts_collection
+from client_src.mongo_db.mongo_model import messages_history_collection, contacts_collection, ignore_list_collection
 
 
 class UserContacts:
@@ -22,3 +22,17 @@ class MessagesHistory:
     def get_history(self, name):
         history = self.collection.find({'$or': [{'from': name}, {'to': name}]})
         return history
+
+
+class IgnoreList:
+    def __init__(self):
+        self.collection = ignore_list_collection
+
+    def add_to_ignore_list(self, name):
+        self.collection.insert_one({'name': name})
+
+    def is_ignored(self, name):
+        checked = self.collection.find({'name': name})
+        if checked:
+            return True
+        return False
--- client_src/mongo_db/mongo_model.py
+++ client_src/mongo_db/mongo_model.py
@@ -5,3 +5,4 @@ data_base = client.client_db
 
 contacts_collection = data_base.user_contacts
 messages_history_collection = data_base.messages_history
+ignore_list_collection = data_base.ignore_list
