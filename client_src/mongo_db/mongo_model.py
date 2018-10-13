c852df93f25a05ccb9dd81c94e8cff16216a8627
StanislavAntunovich <39097798+StanislavAntunovich@users.noreply.github.com>
2018-10-13 21:29:31 +0300

    клиент все же останется на алхимии

--- client_src/mongo_db/mongo_model.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient()
-data_base = client.client_db
-
-contacts_collection = data_base.user_contacts
-messages_history_collection = data_base.messages_history
-ignore_list_collection = data_base.ignore_list
