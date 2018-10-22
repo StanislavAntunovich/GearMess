a6fe321383586e2be62e17ee7c97e28797cd4dc5
StanislavAntunovich <39097798+StanislavAntunovich@users.noreply.github.com>
2018-10-22 21:29:22 +0300

    разметка главного окна + правки по окну ввода данных

--- kivi_client/kivi_gui.py
+++ kivi_client/kivi_gui.py
@@ -1,22 +1,31 @@
 from kivy.app import App
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 
 class SManager(ScreenManager):
 
-    def join_pressed(self):
-        pass
-
     def ok_pressed(self, login, password, action):
-        print(login, '--', password, '==', action)
+        if login and password:
+            pass
+        else:
+            # TODO: добавить кнопку 'OK'
+            pop = Popup(
+                title='Something not filled', content=(Label(text='Please fill all fields')),
+                size_hint=(0.4, 0.4), auto_dismiss=True
+            )
+            pop.open()
 
 
 class GearMessApp(App):
+
     def build(self):
+        Window.size = (410, 500)
         return SManager()
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     GearMessApp().run()
